8a27342d4c8f0494e448cada31a888a9c88faeb0
Curtis Hampton <CurtLHampton@gmail.com>
2020-03-20 19:51:33 -0700

    Format with Black

--- code_zero/code_zero.py
+++ code_zero/code_zero.py
@@ -6,24 +6,21 @@ from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.python_operator import PythonOperator
 
 default_args = {
-    'owner': 'curtis',
-    'depends_on_past': False,
-    'start_date': datetime(2020, 3, 19),
-    'schedule_interval': '@daily',
+    "owner": "curtis",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 3, 19),
+    "schedule_interval": "@daily",
 }
-    
+
 dag = DAG(
-    'get_files_from_s3',
-    default_args=default_args,
-    catchup=True,
-    max_active_runs=1
-    )
+    "get_files_from_s3", default_args=default_args, catchup=True, max_active_runs=1
+)
 
 
 def check_prefix(ds_nodash, **kwargs):
-    hook = S3Hook('aws_s3_lsu')
-    prefix = f'bedpage/{ds_nodash}'
-    check = hook.check_for_prefix('htprawscrapes', prefix, '/')
+    hook = S3Hook("aws_s3_lsu")
+    prefix = f"bedpage/{ds_nodash}"
+    check = hook.check_for_prefix("htprawscrapes", prefix, "/")
     if check == True:
         print(f"The prefix {prefix} exists!")
     else:
@@ -37,17 +34,17 @@ def etl_files(ds_nodash, **kwargs):
     conn.autocommit = True
     cur = conn.cursor()
 
-    hook = S3Hook('aws_s3_lsu')
-    bucket_name = 'htprawscrapes'
-    prefix = f'bedpage/{ds_nodash}/'
+    hook = S3Hook("aws_s3_lsu")
+    bucket_name = "htprawscrapes"
+    prefix = f"bedpage/{ds_nodash}/"
 
     keys = hook.list_keys(bucket_name, prefix=prefix)
-    
+
     print(f"There are {len(keys)} in {prefix}")
 
     for key in keys:
-        s3_key = f's3://{bucket_name}/{key}'
-        data = hook.read_key(key, bucket_name='htprawscrapes')
+        s3_key = f"s3://{bucket_name}/{key}"
+        data = hook.read_key(key, bucket_name="htprawscrapes")
         ad = Bedpage(data)
         x = vars(ad)
 
@@ -56,7 +53,21 @@ def etl_files(ds_nodash, **kwargs):
                 """INSERT INTO ads (s3_key, ad_id, city, category, url, title, body, published_date, modified_date, phone, email, location, age)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 """,
-                [s3_key, x['ad_id'], x['city'], x['category'], x['url'], x['title'], x['body'], x['published_date'], x['modified_date'], x['phone'], x['email'], x['location'], x['age']]
+                [
+                    s3_key,
+                    x["ad_id"],
+                    x["city"],
+                    x["category"],
+                    x["url"],
+                    x["title"],
+                    x["body"],
+                    x["published_date"],
+                    x["modified_date"],
+                    x["phone"],
+                    x["email"],
+                    x["location"],
+                    x["age"],
+                ],
             )
             print(f"{s3_key} inserted into the database")
         except:
@@ -67,17 +78,11 @@ def etl_files(ds_nodash, **kwargs):
 
 
 check_for_prefix = PythonOperator(
-    task_id='check_prefix',
-    python_callable=check_prefix,
-    provide_context=True,
-    dag=dag
+    task_id="check_prefix", python_callable=check_prefix, provide_context=True, dag=dag
 )
 
 etl_files_from_s3 = PythonOperator(
-    task_id='etl_files',
-    python_callable=etl_files,
-    provide_context=True,
-    dag=dag
+    task_id="etl_files", python_callable=etl_files, provide_context=True, dag=dag
 )
 
 check_for_prefix >> etl_files_from_s3
